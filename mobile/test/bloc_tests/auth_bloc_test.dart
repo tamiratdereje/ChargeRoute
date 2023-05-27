bc0e0c4feae44111294e0c80338b6f81315cc20e
tamiratdereje <tamiratdereje167@gmail.com>
2023-05-28 01:49:08 +0300

    add testing for all features

---
+++ mobile/test/bloc_tests/auth_bloc_test.dart
@@ -0,0 +1,94 @@
+import 'package:charge_station_finder/application/auth/auth_bloc.dart';
+import 'package:charge_station_finder/application/core/failure.dart';
+import 'package:charge_station_finder/domain/auth/models/signInFormForm.dart';
+import 'package:charge_station_finder/domain/auth/models/signUpForm.dart';
+import 'package:charge_station_finder/domain/contracts/IAuthRepository.dart';
+import 'package:charge_station_finder/infrastructure/dto/userAuthCredential.dart';
+import 'package:dartz/dartz.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+
+import 'auth_bloc_test.mocks.dart';
+
+// Import the generated mocks file
+// import 'auth_bloc_test.mocks.dart';
+
+// Add the @GenerateMocks annotation to generate mocks
+@GenerateMocks([IAuthenticationRepository])
+void main() {
+  late AuthenticationBloc authenticationBloc;
+  late MockIAuthenticationRepository mockAuthenticationRepository;
+
+  setUp(() {
+    mockAuthenticationRepository = MockIAuthenticationRepository();
+    authenticationBloc = AuthenticationBloc(
+      authRepository: mockAuthenticationRepository,
+    );
+  });
+
+  tearDown(() {
+    authenticationBloc.close();
+  });
+
+  group('AuthenticationBloc', () {
+    test('initial state should be AuthenticationStateInitial', () {
+      expect(authenticationBloc.state, AuthenticationStateInitial());
+    });
+
+    test('should emit AuthenticationLoading and AuthenticationStateUserAuthenticated when LoginEvent is added and login is successful', () {
+      // Arrange
+      final signInForm = SignInForm(
+        email: 'test@example.com',
+        password: 'password123',
+      );
+      final userAuthCredential = UserAuthCredential(
+        id: '123',
+        name: 'John Doe',
+        email: 'test@example.com',
+        role: 'user',
+        password: 'password123',
+        createdAt: '2022-01-01',
+      );
+      final userData = UserData(
+        user: userAuthCredential,
+        token: 'abc123',
+      );
+      when(mockAuthenticationRepository.login(signInForm: signInForm)).thenAnswer((_) async => Right(userData));
+
+      // Act
+      authenticationBloc.add(LoginEvent(signInForm: signInForm));
+
+      // Assert
+      final expectedStates = [
+        AuthenticationLoading(),
+        AuthenticationStateUserAuthenticated(userData: userData),
+      ];
+      expectLater(authenticationBloc.stream, emitsInOrder(expectedStates));
+    });
+
+    test('should emit AuthenticationLoading and AuthenticationStateError when LoginEvent is added and login fails', () {
+      // Arrange
+      final signInForm = SignInForm(
+        email: 'test@example.com',
+        password: 'password123',
+      );
+      final failure = ServerFailure(message: 'Login failed');
+      when(mockAuthenticationRepository.login(signInForm: signInForm)).thenAnswer((_) async => Left(failure));
+
+      // Act
+      authenticationBloc.add(LoginEvent(signInForm: signInForm));
+
+      // Assert
+      final expectedStates = [
+        AuthenticationLoading(),
+        AuthenticationStateError(message: failure.toString()),
+      ];
+      expectLater(authenticationBloc.stream, emitsInOrder(expectedStates));
+    });
+
+    // Add more test cases for other events and scenarios
+
+    // ...
+  });
+}
---
+++ mobile/test/bloc_tests/charge_detail_bloc_test.dart
@@ -0,0 +1,90 @@
+import 'package:charge_station_finder/application/charger_detail/charger_detail_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:mockito/annotations.dart';
+import 'package:charge_station_finder/domain/charger/charger_repository_interface.dart';
+import 'package:charge_station_finder/domain/review/review_repository_interface.dart';
+import 'package:charge_station_finder/common/failure.dart';
+import 'package:charge_station_finder/domain/charger/charger_detail.dart';
+import 'package:dartz/dartz.dart';
+
+// Import the generated mock classes
+import 'charge_detail_bloc_test.mocks.dart';
+
+// Use the @GenerateMocks annotation to generate the mocks
+@GenerateMocks([
+  ChargerRepositoryInterface,
+  ReviewRepositoryInterface,
+])
+void main() {
+  late MockChargerRepositoryInterface chargerRepository;
+  late MockReviewRepositoryInterface reviewRepository;
+  late ChargerDetailBloc chargerDetailBloc;
+
+  setUp(() {
+    chargerRepository = MockChargerRepositoryInterface();
+    reviewRepository = MockReviewRepositoryInterface();
+    chargerDetailBloc = ChargerDetailBloc(
+      chargerRepository: chargerRepository,
+      reviewRepository: reviewRepository,
+    );
+  });
+
+  group('ChargerDetailBloc', () {
+    test('emits ChargerDetailStateLoaded when charger detail loading succeeds', () {
+      final chargerId = 'charger_123';
+      final chargerDetail = ChargerDetail(
+        id: chargerId,
+        description: 'Charger description',
+        phone: '1234567890',
+        name: 'Charger name',
+        address: 'Charger address',
+        rating: 4.5,
+        wattage: 50.0,
+        reviews: [],
+        hasUserRated: false,
+        user: 'User name',
+        userVote: 0,
+      );
+
+      when(chargerRepository.getChargerDetail(chargerId)).thenAnswer((_) async {
+        return Right(chargerDetail);
+      });
+
+      expectLater(
+        chargerDetailBloc.stream,
+        emitsInOrder([
+          isA<ChargerDetailStateLoading>(),
+          ChargerDetailStateLoaded(chargerDetail),
+        ]),
+      );
+      // then((_) {
+      //   verify(chargerRepository.getChargerDetail(chargerId)).called(1);
+      // });
+
+      // chargerDetailBloc.add(ChargerDetailEventLoad(chargerId));
+    });
+
+    test('emits ChargerDetailStateError when charger detail loading fails', () {
+      final chargerId = 'charger_123';
+      final errorMessage = 'Failed to load charger detail';
+
+      when(chargerRepository.getChargerDetail(chargerId)).thenAnswer((_) async {
+        return Left(Failure(errorMessage));
+      });
+
+      expectLater(
+        chargerDetailBloc.stream,
+        emitsInOrder([
+          isA<ChargerDetailStateLoading>(),
+          ChargerDetailStateError(errorMessage),
+        ]),
+      );
+      // then((_) {
+      //   verify(chargerRepository.getChargerDetail(chargerId)).called(1);
+      // });
+
+      // chargerDetailBloc.add(ChargerDetailEventLoad(chargerId));
+    });
+  });
+}
